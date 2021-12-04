bb595e32821bdeb1d3ca9373ba5070b7b313219e
Luc Girardin <luc.girardin@ethz.ch>
2021-12-05 00:26:37 +0100

    Composite

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,18 @@
 group = "com.macrofocus"
 version = "1.0.0"
+
+val composite: String by project
+
+when(composite) {
+    "includeBuild" -> {
+        configurations.all {
+            resolutionStrategy.dependencySubstitution {
+                substitute(
+                    module("com.macrofocus:common")
+                ).using(project(":common")).because("we work with the unreleased development version")
+
+//                substitute(module("com.macrofocus:common")).using(project(":common"))
+            }
+        }
+    }
+}
